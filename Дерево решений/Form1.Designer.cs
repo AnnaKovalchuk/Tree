74f65ecdd87b9e92f35363fe83986d524bed88e0
anaykovalchuk00@mail.ru <anaykovalchuk00@mail.ru>
2022-05-27 12:22:07 +0300

    Добавьте файлы проекта.

---
+++ Дерево решений/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace Дерево_решений
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxAnswer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxAnswer
+            // 
+            this.textBoxAnswer.Location = new System.Drawing.Point(489, 136);
+            this.textBoxAnswer.Name = "textBoxAnswer";
+            this.textBoxAnswer.Size = new System.Drawing.Size(100, 22);
+            this.textBoxAnswer.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(502, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Решение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Выберите количество стратегий";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(13, 30);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(220, 22);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(12, 59);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(221, 29);
+            this.buttonEnter.TabIndex = 3;
+            this.buttonEnter.Text = "Применить";
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 289F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(118, 557);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(861, 100);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(403, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(221, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1278, 705);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxAnswer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxAnswer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Дерево решений/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Дерево_решений
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        List<TextBox> textBoxes = new List<TextBox>();
+        List<DataGridView> dataGridViewes = new List<DataGridView>();
+        private void CreateTextBox(int count)
+        {
+            int x = (textBoxAnswer.Location.X / (int)numericUpDown1.Value) + 70;
+            for (int i = 0; i < count; i++)
+            {
+                string name = "textBox" + i.ToString();
+                string name2 = "dataGridView" + i.ToString();
+
+                TextBox textBox = new TextBox();
+                DataGridView dataGridView = new DataGridView();
+
+                textBox.Name = name;
+                dataGridView.Name = name2;
+
+                textBox.Width = 40;
+                textBox.Height = 20;
+
+
+                dataGridView.Width = 200;
+                dataGridView.Height = 200;
+                dataGridView.Columns.Add("Вероятности", "Количество посетителей");
+                dataGridView.Columns.Add("Деньги", "Доход");
+                dataGridView.RowHeadersVisible = false;
+
+                int y = 50;
+                textBox.Location = new Point(x + x*i, textBoxAnswer.Location.Y + y);
+                dataGridView.Location = new Point(x + x * i - 75, textBoxAnswer.Location.Y + y + 70);
+                
+
+                Controls.Add(textBox);
+                Controls.Add(dataGridView);
+                textBoxes.Add(textBox);
+                dataGridViewes.Add(dataGridView);
+            }
+        }
+
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+
+            CreateTextBox((int)numericUpDown1.Value);
+
+           
+                
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<double> dd = new List<double>();
+            double a = 0;
+            double b = 0;
+            double c = 0;
+            double d = 0;
+            for (int i = 0; i < dataGridViewes.Count(); i++)
+            {
+                for (int j = 0; j < dataGridViewes[i].RowCount-1; j++)
+                {
+                    a = Convert.ToDouble(dataGridViewes[i].Rows[j].Cells[0].Value);
+                    b = Convert.ToDouble(dataGridViewes[i].Rows[j].Cells[1].Value);
+                    c = a * b;
+                    d += c;
+                }
+                textBoxes[i].Text = d.ToString();
+                dd.Add(d);
+                d=0;
+            }
+            for (int i = 0; i < textBoxes.Count(); i++)
+            {
+                textBoxAnswer.Text= dd.Max().ToString();
+            }
+        }
+    }
+}
